e8c6161bdaf18a1b4c5c327894e079847d5c9b3f
Kostya <kostazadoroznij9@gmail.com>
2022-12-19 22:34:48 +0200

    Code refactoring

--- LB_8/lb_8_a.cpp
+++ LB_8/lb_8_a.cpp
@@ -4,71 +4,32 @@
 
 using namespace std;
 
-
 int get_random_number(int start, int end);
-void print_array(int array[][6], int height, int width);
+
+template< typename T, size_t N, size_t M >
+void print_array(T(&array)[N][M]);
+
+template<size_t N, size_t M >
+void fill_array(int(&array)[N][M]);
+
+template<size_t N, size_t M >
+void fill_zeros(int(&array)[N][M]);
 
 int main()
 {
     srand(time(NULL));
+    int array[4][6];
 
-    int height = 4;
-    int width = 6;
-    int array[height][width];
+    fill_array(array);
+    print_array(array);
 
+    fill_zeros(array);
 
-
-    for(int i = 0; i < height; i++)
-    {
-        for(int j = 0; j < width; j++)
-        {
-            array[i][j] = get_random_number(0, 1000);
-        }
-    }
-
-    int max = array[0][0];
-    int min = array[0][0];
-
-    for(int i = 0; i < height; i++)
-    {
-        for(int j = 0; j < width; j ++){
-            cout << array[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    for(int i = 0; i < height; i++)
-    {
-        for(int j = 0; j < width; j ++){
-            if(max < array[i][j]){
-                max = array[i][j];
-            }
-            if(min > array[i][j]){
-                min = array[i][j];
-            }
-        }
-    }
     cout << endl
          << "New array: " << endl;
-    for(int i = 0; i < height; i++)
-    {
-        for(int j = 0; j < width; j ++){
-            if(max != array[i][j] && min != array[i][j]){
-                array[i][j] = 0;
-            }
+    print_array(array);
 
-        }
-    }
-
-    for(int i = 0; i < height; i++)
-    {
-        for(int j = 0; j < width; j ++){
-            cout << array[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-
+    return 0;
 }
 
 int get_random_number(int start, int end)
@@ -76,4 +37,60 @@ int get_random_number(int start, int end)
     return start + (rand() % end);
 }
 
+template< typename T, size_t N, size_t M >
+void print_array(T(&array)[N][M])
+{
+    for(int i = 0; i < (int)N; i++)
+    {
+        for(int j = 0; j < (int)M; j ++)
+        {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
+template<size_t N, size_t M >
+void fill_array(int(&array)[N][M])
+{
+    for(int i = 0; i < (int)N; i++)
+    {
+        for(int j = 0; j < (int)M; j++)
+        {
+            array[i][j] = get_random_number(0, 1000);
+        }
+    }
+}
+
+template<size_t N, size_t M >
+void fill_zeros(int(&array)[N][M])
+{
+    int max = array[0][0];
+    int min = array[0][0];
+
+    for(int i = 0; i < (int)N; i++)
+    {
+        for(int j = 0; j < (int)M; j ++)
+        {
+            if(max < array[i][j])
+            {
+                max = array[i][j];
+            }
+            if(min > array[i][j])
+            {
+                min = array[i][j];
+            }
+        }
+    }
+
+    for(int i = 0; i < (int)N; i++)
+    {
+        for(int j = 0; j < (int)M; j ++)
+        {
+            if(max != array[i][j] && min != array[i][j])
+            {
+                array[i][j] = 0;
+            }
+        }
+    }
+}
