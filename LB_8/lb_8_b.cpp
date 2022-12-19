8401348a79369e7dff6c546c0660fee80e2d991e
Kostya <kostazadoroznij9@gmail.com>
2022-12-19 22:43:27 +0200

    Code refactoring

--- LB_8/lb_8_b.cpp
+++ LB_8/lb_8_b.cpp
@@ -7,54 +7,81 @@ using namespace std;
 
 int get_random_number(int start, int end);
 
+template< typename T, size_t N, size_t M >
+void print_array(T(&array)[N][M]);
+
+template<size_t N, size_t M >
+void fill_array(int(&array)[N][M]);
+
+template<size_t N, size_t M >
+int get_sum_triangles(int(&array)[N][M]);
+
 // Task B variant A
 int main()
 {
-    srand(time(NULL));
+    int array[10][9];
 
-    int height = 10;
-    int width = 9;
-    int array[height][width];
+    fill_array(array);
+    print_array(array);
 
+    cout << endl << "Sum: " << get_sum_triangles(array);
 
+    return 0;
 
-    for(int i = 0; i < height; i++)
-    {
-        for(int j = 0; j < width; j++)
-        {
-            array[i][j] = get_random_number(0, 10);
-        }
-    }
-
-    for(int i = 0; i < height; i++)
-    {
-        for(int j = 0; j < width; j ++){
-            cout << array[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    // Sum elements variant A
-    int sum = 0;
-    for(int i = 0; i < height/2; i++) // 0 < i < 5
-    {
-        for(int j = i; j < width - i; j ++){
-            sum += array[i][j];
-        }
-    }
-    for(int i = height/2; i < height; i++) // 5 < i < 10
-    {
-        for(int j = height - i - 1; j < width - height + i + 1; j ++){
-            sum += array[i][j];
-        }
-    }
-
-    cout << endl << "Sum: " << sum;
 }
 
 int get_random_number(int start, int end)
 {
     return start + (rand() % end);
+}
+
+template<typename T, size_t N, size_t M>
+void print_array(T(&array)[N][M])
+{
+    for(int i = 0; i < (int)N; i++)
+    {
+        for(int j = 0; j < (int)M; j ++)
+        {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+template<size_t N, size_t M >
+void fill_array(int(&array)[N][M])
+{
+    srand(time(NULL));
+    for(int i = 0; i < (int)N; i++)
+    {
+        for(int j = 0; j < (int)M; j++)
+        {
+            array[i][j] = get_random_number(0, 10);
+        }
+    }
+}
+
+template<size_t height, size_t width >
+int get_sum_triangles(int(&array)[height][width])
+{
+    // Sum elements variant A
+    int sum = 0;
+    for(int i = 0; i < (int)height/2; i++) // 0 < i < 5
+    {
+        for(int j = i; j < (int)width - i; j ++)
+        {
+            sum += array[i][j];
+        }
+    }
+    for(int i = height/2; i < (int)height; i++) // 5 < i < 10
+    {
+        for(int j = height - i - 1; j < (int)width - (int)height + i + 1; j ++)
+        {
+            sum += array[i][j];
+        }
+    }
+
+    return sum;
 }
 
 
