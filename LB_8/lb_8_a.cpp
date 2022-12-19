e06c9f3bf05a18747d6ec9ff88811eda82a46f3b
Kostya <kostazadoroznij9@gmail.com>
2022-12-19 22:58:46 +0200

    Added a comment to the code

--- LB_8/lb_8_a.cpp
+++ LB_8/lb_8_a.cpp
@@ -1,23 +1,33 @@
-#include <iostream>
+
+3#include <iostream>
 #include <time.h>
 #include <cstdlib>
 
 using namespace std;
 
+// Отримання випадкового значення
 int get_random_number(int start, int end);
 
-template< typename T, size_t N, size_t M >
+// Вивід в консоль будь якого типу масиву
+// typename T - означає будь який тип,
+// t — це тип, здатний представляти розмір будь-якого об’єкта в байтах:
+// size_t — це тип, який повертає оператор sizeof і широко використовується в стандартній бібліотеці
+// для представлення розмірів і кількості.
+// N та М - розміри масиву, які автоматично визначаються при передані інформації в функцію
+// https://metanit.com/cpp/tutorial/9.2.php
+template<typename T, size_t N, size_t M>
 void print_array(T(&array)[N][M]);
 
-template<size_t N, size_t M >
+// Заповнення двомірного масиву рандомними числами
+template<size_t N, size_t M>
 void fill_array(int(&array)[N][M]);
 
+// Виконання саомго завдання, Заповнення всіх крім максимального та мінімального елемента нулями
 template<size_t N, size_t M >
 void fill_zeros(int(&array)[N][M]);
 
 int main()
 {
-    srand(time(NULL));
     int array[4][6];
 
     fill_array(array);
@@ -53,6 +63,7 @@ void print_array(T(&array)[N][M])
 template<size_t N, size_t M >
 void fill_array(int(&array)[N][M])
 {
+    srand(time(NULL));
     for(int i = 0; i < (int)N; i++)
     {
         for(int j = 0; j < (int)M; j++)
@@ -65,17 +76,22 @@ void fill_array(int(&array)[N][M])
 template<size_t N, size_t M >
 void fill_zeros(int(&array)[N][M])
 {
+    // Ініціалізуємо максимальний та мінімальний елемент першим елементом масива
     int max = array[0][0];
     int min = array[0][0];
 
+
+    // Шукаємо мінімальний та максимальний елемент
     for(int i = 0; i < (int)N; i++)
     {
         for(int j = 0; j < (int)M; j ++)
         {
+            // Пошук максимального числа
             if(max < array[i][j])
             {
                 max = array[i][j];
             }
+            // Пошук мінімального числа
             if(min > array[i][j])
             {
                 min = array[i][j];
@@ -83,10 +99,12 @@ void fill_zeros(int(&array)[N][M])
         }
     }
 
+    // Алгоритм заповнення всіх інших чисел на нулі
     for(int i = 0; i < (int)N; i++)
     {
         for(int j = 0; j < (int)M; j ++)
         {
+            // Якщо число не дорівнює ні максимальному, ні мінімальному то заповнюємо його нулем
             if(max != array[i][j] && min != array[i][j])
             {
                 array[i][j] = 0;
